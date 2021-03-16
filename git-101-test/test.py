62cf2dc2dc11587209fb35b77ba2550af54ed2f3
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2021-03-16 12:47:40 +0100

    Fixed/removed sub repos

---
+++ git-101-test/test.py
@@ -0,0 +1,16 @@
+# ramusmethod for life
+
+def recur(indict={1:'hej'}):
+	print(indict)
+	indict[i] = i * 2
+
+name = "Rasmus"
+def this_is_my_branc(input):
+	print(f"This branch belongs to {name}")
+
+for i in range(5):
+	this_is_my_branch(name)
+
+for i in range(100):
+	recur()
+
