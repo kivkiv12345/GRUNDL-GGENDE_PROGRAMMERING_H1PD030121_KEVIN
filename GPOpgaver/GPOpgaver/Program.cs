927810209ab8219a2af89cc02db7aca933b6b6ba
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2021-03-16 12:50:32 +0100

    Fixed fixed repo

---
+++ GPOpgaver/GPOpgaver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static GPOpgaver.Opgaver;
+
+namespace GPOpgaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = 1;
+            int b = 2;
+            Console.WriteLine($"{a}, {b}");
+            Interchange(ref a, ref b);
+            Console.WriteLine($"{a}, {b}");*/
+
+            /*Console.WriteLine($"{15:00000}");
+            int i = 1;
+            Console.WriteLine(i.ToString(new string('0', 10)));*/
+        }
+    }
+}
