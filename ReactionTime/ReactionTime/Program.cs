6b179a045bde92de9f6e739eed7ee730ef7f508a
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2021-03-18 08:37:02 +0100

    Added ReactionTime game (Currently without fail state)

---
+++ ReactionTime/ReactionTime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ReactionTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start:");
+            Console.ReadKey();
+            Console.WriteLine("Press ENTER when O appears on the screen");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Random random = new Random();
+            Stopwatch stopWatch = new Stopwatch();
+            TimeSpan time;
+
+            Thread.Sleep(random.Next(1000, 8001));
+            Console.WriteLine("O");
+            stopWatch.Start();
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            while (key.Key != ConsoleKey.Enter)
+                key = Console.ReadKey();
+            stopWatch.Stop();
+            time = stopWatch.Elapsed;
+
+            Console.Clear();
+            Console.WriteLine($"It took you: {time} seconds to press ENTER.");
+            Console.ReadLine();
+
+        }
+    }
+}
